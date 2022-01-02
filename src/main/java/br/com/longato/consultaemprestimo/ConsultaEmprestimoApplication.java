b0afdbeb9acffc4e67408b038a48da11ab29b820
Eduardo <edulongato@yahoo.com.br>
2022-01-02 18:53:07 -0300

    Initial commit

---
+++ src/main/java/br/com/longato/consultaemprestimo/ConsultaEmprestimoApplication.java
@@ -0,0 +1,13 @@
+package br.com.longato.consultaemprestimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaEmprestimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaEmprestimoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/longato/consultaemprestimo/ConsultaEmprestimoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.longato.consultaemprestimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaEmprestimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
