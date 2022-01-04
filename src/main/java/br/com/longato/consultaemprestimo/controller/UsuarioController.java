f3adc27a555d2b8554c9e7c7e5a9c86300635fb1
Eduardo <edulongato@yahoo.com.br>
2022-01-04 15:30:08 -0300

    Implementadas as classes Controller e Services.

--- src/main/java/br/com/longato/consultaemprestimo/controller/UsuarioController.java
+++ src/main/java/br/com/longato/consultaemprestimo/controller/UsuarioController.java
@@ -1,15 +1,52 @@
 package br.com.longato.consultaemprestimo.controller;
 
-import br.com.longato.consultaemprestimo.repository.UsuarioRepository;
+import br.com.longato.consultaemprestimo.model.Usuario;
+import br.com.longato.consultaemprestimo.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
+@RequestMapping("/user")
 public class UsuarioController {
 
     @Autowired
-    private UsuarioRepository usuarioRepository;
+    private UsuarioService usuarioService;
 
+    @PostMapping
+    public Usuario createUsuario(@RequestBody Usuario usuario){
+        return usuarioService.saveUsuario(usuario);
+    }
+
+    @GetMapping
+    public List<Usuario> getUsuarioList(){
+        return usuarioService.findAll();
+    }
+
+    @GetMapping("/{idUsuario}")
+    public ResponseEntity<Usuario> getUsuarioById(@PathVariable("idUsuario") Long idUsuario) throws Exception{
+        return ResponseEntity.ok(usuarioService.getById(idUsuario).orElseThrow(() -> new NoSuchElementException("Usuario nao encontrado")));
+    }
+
+    @PutMapping
+    public Usuario updateUsuario(@RequestBody Usuario usuario){
+        return usuarioService.updateUsuario(usuario);
+    }
+
+    @DeleteMapping("/{idUsuario}")
+    @ResponseBody
+    public ResponseEntity<Usuario> deleteById(@PathVariable("idUsuario") Long idUsuario) throws Exception{
+        try{
+            usuarioService.deleteUsuario(idUsuario);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return(ResponseEntity<Usuario>) ResponseEntity.ok();
+
+    }
 
 
 }
--- src/main/java/br/com/longato/consultaemprestimo/service/UsuarioService.java
+++ src/main/java/br/com/longato/consultaemprestimo/service/UsuarioService.java
@@ -1,7 +1,44 @@
 package br.com.longato.consultaemprestimo.service;
 
+import br.com.longato.consultaemprestimo.model.Usuario;
+import br.com.longato.consultaemprestimo.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
+
+    UsuarioRepository usuarioRepository;
+
+    @Autowired
+    public UsuarioService(UsuarioRepository usuarioRepository){
+        this.usuarioRepository = usuarioRepository;
+    }
+
+
+    public Usuario saveUsuario(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
+    public List<Usuario> findAll(){
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> getById(Long idUsuario){
+        return usuarioRepository.findById(idUsuario);
+    }
+
+    public Usuario updateUsuario(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
+    public void deleteUsuario(Long idUsuario){
+        usuarioRepository.deleteById(idUsuario);
+    }
+
+
+
 }
